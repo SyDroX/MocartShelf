1e46f07f808e0e7f9b11faea3f30880859d61634
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2024-11-10 18:41:41 +0200

    Previous commit failed to add selector logic, adding it again

--- Assets/Scripts/UI/ProductSelector.cs
+++ Assets/Scripts/UI/ProductSelector.cs
@@ -1,9 +1,77 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using Entities;
+using UniRx;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace UI
 {
     public class ProductSelector : MonoBehaviour
     {
+        private IDisposable _receiver;
+        private List<Product> _products;
+        private int _selectedIndex;
         
+        [SerializeField] private Button _leftButton;
+        [SerializeField] private Button _rightButton;
+        [SerializeField] private Button _editButton;
+        
+        private void OnEnable()
+        {
+            _editButton.onClick.AddListener(OnEdit);
+            _rightButton.onClick.AddListener(OnRight);
+            _leftButton.onClick.AddListener(OnLeft);
+            _receiver = MessageBroker.Default.Receive<List<Product>>().ObserveOnMainThread().Subscribe(OnProductsReceived);
+        }
+
+        private void OnDisable()
+        {
+            _editButton.onClick.RemoveListener(OnEdit);
+            _rightButton.onClick.RemoveListener(OnRight);
+            _leftButton.onClick.RemoveListener(OnLeft);
+            _receiver.Dispose();
+        }
+
+        private void OnEdit()
+        {
+            MessageBroker.Default.Publish(_products[_selectedIndex]);
+        }
+
+        private void OnLeft()
+        {
+            _selectedIndex -= 1 % _products.Count;
+            MessageBroker.Default.Publish(_products[_selectedIndex].ProductInfo);
+        }
+
+        private void OnRight()
+        {
+            _selectedIndex += 1 % _products.Count;
+            MessageBroker.Default.Publish(_products[_selectedIndex].ProductInfo);
+        }
+
+        private void ToggleButtons(bool state)
+        {
+            _rightButton.gameObject.SetActive(state);
+            _leftButton.gameObject.SetActive(state);
+        }
+
+        private void OnProductsReceived(List<Product> products)
+        {
+            _products = products;
+            
+            if (_products.Count == 1)
+            {
+                ToggleButtons(false);
+                _selectedIndex = 0;
+            }
+            else
+            {
+                ToggleButtons(true);
+                _selectedIndex = 1;
+            }
+            
+            MessageBroker.Default.Publish(_products[_selectedIndex].ProductInfo);
+        }
     }
 }

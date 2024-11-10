c5e6772a3181f4c534c98c7d5ac719092bb12c28
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2024-11-10 14:58:53 +0200

    Added product display logic

---
+++ Assets/Scripts/ProductDisplayer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+public class ProductDisplayer : MonoBehaviour
+{
+    private IDisposable   _receiver;
+    private List<Product> _shownProducts = new();
+    
+    [SerializeField] private Vector3 _initialPosition;
+    [SerializeField] private float   _offsetX;
+    
+    private void OnEnable()
+    {
+        _receiver = MessageBroker.Default.Receive<List<Product>>().ObserveOnMainThread().Subscribe(OnProductsReceived);
+    }
+    
+    private void OnDisable()
+    {
+        _receiver.Dispose();
+    }
+
+    private void OnProductsReceived(List<Product> products)
+    {
+        _shownProducts.ForEach(sp => sp.GameObject.SetActive(false));
+        _shownProducts = new List<Product>(products);
+
+        for (var index = 0; index < _shownProducts.Count; index++)
+        {
+            Product product = _shownProducts[index];
+            product.GameObject.transform.position = _initialPosition + new Vector3(_offsetX * index, 0, 0);
+            product.GameObject.SetActive(true);
+        }
+    }
+}

2f7e70503b46a2b2dad9e51e393e8b92c8aff1d8
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2024-11-10 13:44:45 +0200

    Extracted rotator logic to sperate script, added error handling, renamed InstantiateProducts to InstantiateProductsPool

--- Assets/Scripts/ProductSpawner.cs
+++ Assets/Scripts/ProductSpawner.cs
@@ -1,13 +1,12 @@
 using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 using WebRequests;
 
 public class ProductSpawner : MonoBehaviour
 {
-    [SerializeField] private Vector3 _initialPosition;
-    [SerializeField] private float   _offsetX;
-    [SerializeField] private float   _rotationSpeed = 1f;
-    
+    [SerializeField] private Vector3      _initialPosition;
+    [SerializeField] private float        _offsetX;
     [SerializeField] private GameObject[] _productPrefabs;
 
     private GameObject[] _productsPool;
@@ -16,47 +15,50 @@ public class ProductSpawner : MonoBehaviour
     private void Start()
     {
         _spawnedProductIndexes = new List<int>();
-        InstantiateProducts();
+        InstantiateProductsPool();
         SpawnProducts();
-    }
-    private void Update()
-    {
-        _spawnedProductIndexes.ForEach(sp=> _productsPool[sp].transform.Rotate(new Vector3(0,1)* (Time.deltaTime * _rotationSpeed)));
     }
 
     public async void SpawnProducts()
     {
         _spawnedProductIndexes.Clear();
-        
+
         foreach (GameObject product in _productsPool)
         {
             product.SetActive(false);
         }
-        
-        Product[] products = await ProductHandler.Get();
 
+        // TODO: error handling
+        Product[] products = await ProductHandler.Get();
+        
         for (var i = 0; i < products.Length; i++)
         {
             if (int.TryParse(products[i].Name.Split(' ')[1], out int productIndex))
             {
-                // Convert from displayed to array
+                // Convert from displayed to zero based array
                 productIndex -= 1;
-                
-                _productsPool[productIndex].transform.position =  _initialPosition + new Vector3(_offsetX * i, 0, 0);
+
+                _productsPool[productIndex].transform.position = _initialPosition + new Vector3(_offsetX * i, 0, 0);
                 _productsPool[productIndex].SetActive(true);
                 _spawnedProductIndexes.Add(productIndex);
             }
+            else
+            {
+                Debug.LogError("Error phrasing product number");
+            }
         }
+        
+        MessageBroker.Default.Publish(new List<Transform>());
     }
 
-    private void InstantiateProducts()
+    private void InstantiateProductsPool()
     {
         _productsPool = new GameObject[_productPrefabs.Length];
         Transform productPoolRoot = new GameObject("ProductPool").transform;
-        
-        for (int i = 0; i < _productPrefabs.Length; i++)
+
+        for (var i = 0; i < _productPrefabs.Length; i++)
         {
-            _productsPool[i] = Instantiate(_productPrefabs[i],productPoolRoot);
+            _productsPool[i] = Instantiate(_productPrefabs[i], productPoolRoot);
         }
     }
 }
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Rotator : MonoBehaviour
+    {
+        [SerializeField] private float _rotationSpeed = 1f;
+        
+        private void Update()
+        {
+            transform.Rotate(Vector3.up * (Time.deltaTime * _rotationSpeed));
+        }
+    }
+}

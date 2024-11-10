8da378993c986f9714abde6b7b98c616ae1f87e1
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2024-11-10 17:07:46 +0200

    Added logic to display the product in the middle if its only one

--- Assets/Scripts/ProductDisplayer.cs
+++ Assets/Scripts/ProductDisplayer.cs
@@ -21,17 +21,26 @@ public class ProductDisplayer : MonoBehaviour
     {
         _receiver.Dispose();
     }
-
+    
     private void OnProductsReceived(List<Product> products)
     {
         _shownProducts.ForEach(sp => sp.GameObject.SetActive(false));
         _shownProducts = new List<Product>(products);
 
-        for (var index = 0; index < _shownProducts.Count; index++)
+        if (_shownProducts.Count == 1)
         {
-            Product product = _shownProducts[index];
-            product.GameObject.transform.position = _initialPosition + new Vector3(_offsetX * index, 0, 0);
+            Product product = _shownProducts[0];
+            product.GameObject.transform.position = _initialPosition + new Vector3(_offsetX, 0, 0);
             product.GameObject.SetActive(true);
+        }
+        else
+        {
+            for (var index = 0; index < _shownProducts.Count; index++)
+            {
+                Product product = _shownProducts[index];
+                product.GameObject.transform.position = _initialPosition + new Vector3(_offsetX * index, 0, 0);
+                product.GameObject.SetActive(true);
+            }
         }
     }
 }

f691fc7d4d53544949b93ae8fe72b6d7af628467
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2024-11-11 17:55:30 +0200

    Added comments

--- Assets/Scripts/WebRequests/HttpClientHandler.cs
+++ Assets/Scripts/WebRequests/HttpClientHandler.cs
@@ -2,6 +2,8 @@
 
 namespace WebRequests
 {
+    // I opted to use .Net's http client for web requests. This is because it allows better control of API request life-cycle
+    // In addition, it allows to run these requests in async interdependently on unity's life-cycle.
     public sealed class HttpClientHandler
     {
         public readonly HttpClient SharedClient;
@@ -19,6 +21,8 @@ namespace WebRequests
             SharedClient = new HttpClient();
         }
 
+        // Unity's Editor life-cycle can't properly dispose of a shared client instance this persists in version 2019 to 2022,
+        // Maybe it's fixed in the new version 6, haven't tested yet. However, this works fine in build.
 #if !UNITY_EDITOR
         ~HttpClientHandler()
         {

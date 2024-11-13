d24f32b01293c2b7ee7851955497e94697113593
Kostya Sydoruk <Sydoruk.kostya@gmail.com>
2024-11-13 17:19:23 +0200

    Added a flag to disable server spamming

--- Assets/Scripts/WebRequests/ProductLoader.cs
+++ Assets/Scripts/WebRequests/ProductLoader.cs
@@ -7,118 +7,129 @@ using Newtonsoft.Json;
 using UniRx;
 using UnityEngine;
 using UnityEngine.Networking;
-using WebRequests;
 
-public class ProductLoader : MonoBehaviour
+namespace WebRequests
 {
-    private const string ApiUri          = "https://homework.mocart.io/api/products";
-    private const string ProductRequestError = "Error getting products from server!";
-
-    private IDisposable         _receiver;
-    private MocratProduct[]     _products;
-    private List<MocratProduct> _loadedProducts = new();
-
-    private void OnEnable()
+    public class ProductLoader : MonoBehaviour
     {
-        _receiver = MessageBroker.Default.Receive<ProductsPooledEventArgs>().ObserveOnMainThread().Subscribe(OnProductPoolCreated);
-    }
+        // Since there is only one entry point to access the API I'm hardcoding the uri, this should come form a configuration file
+        // and have a base uri and uri pages for each api method.
+        private const string ApiUri              = "https://homework.mocart.io/api/products";
+        private const string ProductRequestError = "Error getting products from server!";
 
-    private void OnDisable()
-    {
-        _receiver.Dispose();
-    }
+        private bool                _loadInProgress;
+        private IDisposable         _receiver;
+        private MocratProduct[]     _products;
+        private List<MocratProduct> _loadedProducts = new();
 
-    private void OnProductPoolCreated(ProductsPooledEventArgs args)
-    {
-        _products = args.ProductsPool;
-        LoadProducts();
-    }
-
-    private void TryAddProduct(ProductInfo productInfo)
-    {
-        if (int.TryParse(productInfo.Name.Split(' ')[1], out int productIndex))
+        private void OnEnable()
         {
-            // Convert from displayed to zero based array
-            productIndex -= 1;
-            // Using the product name as ID, this allows the changes made to product info to persist between reloads
-            _products[productIndex].ProductInfo ??= productInfo;
-            _loadedProducts.Add(_products[productIndex]);
+            _receiver = MessageBroker.Default.Receive<ProductsPooledEventArgs>().ObserveOnMainThread().Subscribe(OnProductPoolCreated);
         }
-        else
+
+        private void OnDisable()
         {
-            var errorMessage = $"Error, Product '{productInfo.Name}' is not a number";
-            Debug.LogError(errorMessage);
-            MessageBroker.Default.Publish(new MessageEventArgs
-            {
-                Message     = errorMessage,
-                MessageType = MessageType.Error
-            });
+            _receiver.Dispose();
         }
-    }
 
-    private void HandleServerResponse(string productData)
-    {
-        try
+        private void OnProductPoolCreated(ProductsPooledEventArgs args)
         {
-            ProductInfo[] productInfos = ProductJsonHelper.Deserialize(productData);
-            _loadedProducts.Clear();
+            _products = args.ProductsPool;
+            LoadProducts();
+        }
 
-            foreach (ProductInfo productInfo in productInfos)
+        private void TryAddProduct(ProductInfo productInfo)
+        {
+            if (int.TryParse(productInfo.Name.Split(' ')[1], out int productIndex))
             {
-                TryAddProduct(productInfo);
+                // Convert from displayed to zero based array
+                productIndex -= 1;
+                // Using the product name as ID, this allows the changes made to product info to persist between reloads
+                _products[productIndex].ProductInfo ??= productInfo;
+                _loadedProducts.Add(_products[productIndex]);
             }
-
-            MessageBroker.Default.Publish(new LoadedProductsEventArgs { LoadedProducts = _loadedProducts });
-        }
-        catch (JsonException jsonException)
-        {
-            MessageBroker.Default.Publish(new MessageEventArgs
+            else
             {
-                Message     = "Error reading product data!",
-                MessageType = MessageType.Error
-            });
-            Debug.LogError(jsonException.Message + "\n" + jsonException.StackTrace);
+                var errorMessage = $"Error, Product '{productInfo.Name}' is not a number";
+                Debug.LogError(errorMessage);
+                MessageBroker.Default.Publish(new MessageEventArgs
+                {
+                    Message     = errorMessage,
+                    MessageType = MessageType.Error
+                });
+            }
         }
-        catch (Exception exception)
-        {
-            MessageBroker.Default.Publish(new MessageEventArgs
-            {
-                Message     = "Unknown Error!",
-                MessageType = MessageType.Error
-            });
-            Debug.LogError(exception.Message + "\n" + exception.StackTrace);
-        }
-        finally
-        {
-            MessageBroker.Default.Publish(new LoadingPanelEventArgs { Enabled = false });
-        }
-    }
 
-    private IEnumerator RequestProductsFromServer(string uri)
-    {
-        MessageBroker.Default.Publish(new LoadingPanelEventArgs { Enabled = true });
+        private void HandleServerResponse(string productData)
+        {
+            try
+            {
+                ProductInfo[] productInfos = ProductJsonHelper.Deserialize(productData);
+                _loadedProducts.Clear();
+
+                foreach (ProductInfo productInfo in productInfos)
+                {
+                    TryAddProduct(productInfo);
+                }
+
+                MessageBroker.Default.Publish(new LoadedProductsEventArgs { LoadedProducts = _loadedProducts });
+            }
+            catch (JsonException jsonException)
+            {
+                MessageBroker.Default.Publish(new MessageEventArgs
+                {
+                    Message     = "Error reading product data!",
+                    MessageType = MessageType.Error
+                });
+                Debug.LogError(jsonException.Message + "\n" + jsonException.StackTrace);
+            }
+            catch (Exception exception)
+            {
+                MessageBroker.Default.Publish(new MessageEventArgs
+                {
+                    Message     = "Unknown Error!",
+                    MessageType = MessageType.Error
+                });
+                Debug.LogError(exception.Message + "\n" + exception.StackTrace);
+            }
+            finally
+            {
+                MessageBroker.Default.Publish(new LoadingPanelEventArgs { Enabled = false });
+                _loadInProgress = false;
+            }
+        }
+
+        private IEnumerator RequestProductsFromServer(string uri)
+        {
+            MessageBroker.Default.Publish(new LoadingPanelEventArgs { Enabled = true });
         
-        using UnityWebRequest webRequest = UnityWebRequest.Get(uri);
+            using UnityWebRequest webRequest = UnityWebRequest.Get(uri);
         
-        yield return webRequest.SendWebRequest();
+            yield return webRequest.SendWebRequest();
 
-        if (webRequest.Succeeded())
-        {
-            HandleServerResponse(webRequest.downloadHandler.text);
-        }
-        else
-        {
-            Debug.LogError(uri + " " + webRequest.result + ": Error: " + webRequest.error);
-            MessageBroker.Default.Publish(new MessageEventArgs
+            if (webRequest.Succeeded())
             {
-                Message     = ProductRequestError,
-                MessageType = MessageType.Error
-            });
+                HandleServerResponse(webRequest.downloadHandler.text);
+            }
+            else
+            {
+                _loadInProgress = false;
+                Debug.LogError(uri + " " + webRequest.result + ": Error: " + webRequest.error);
+                MessageBroker.Default.Publish(new MessageEventArgs
+                {
+                    Message     = ProductRequestError,
+                    MessageType = MessageType.Error
+                });
+            }
         }
-    }
 
-    public void LoadProducts()
-    {
-        StartCoroutine(RequestProductsFromServer(ApiUri));
+        public void LoadProducts()
+        {
+            if (!_loadInProgress)
+            {
+                _loadInProgress = true;
+                StartCoroutine(RequestProductsFromServer(ApiUri));    
+            }
+        }
     }
 }
